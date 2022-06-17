0f487869b1ce1f855faad431b9248a1a06c28208
João Victor Nascimento <66145032+joaovictorjvn@users.noreply.github.com>
2022-06-17 19:08:03 -0300

    50 49 - Correção - Inicio agora parte de 0 ao invés de 1 - OBS: O enunciado exigia a incrementação dos salário, eu falhei na interpretção da última vez.

--- 49.py
+++ 49.py
@@ -1,6 +1,6 @@
 salc = float(input("Informe o salário de Carlos: R$ "))
 salj = round(salc * (1 / 3), 10)
-meses = 1
+meses = 0
 rendc = 0
 rendj = 0
 while True:
@@ -15,8 +15,3 @@ while True:
         break
     else:
         meses = meses + 1
-"""
-Nota: O enunciado não informa se o salário precisa ser incrementado, porém o programa fica em
-loop infinito se o salário não for incrementado, por isso optei por deixar o programa com o 
-salário sendo incrementado.
-"""
---
+++ 50.py
@@ -0,0 +1,8 @@
+altc = 1.50
+altz = 1.10
+anos = 0
+while altz <= altc:
+    altc = altc + 0.02
+    altz = altz + 0.03
+    anos = anos + 1
+print(f'Serão necessários {anos} anos para Zé ser maior do que Chico.')
